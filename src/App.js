40e9513884b92f8f295d79d8fccdf992a6271473
Josh Fuller <dorothy@joshs-mbp.lan>
2021-02-05 17:22:20 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { Route, Switch } from "react-router-dom";
 
+import "./App.css";
+import Welcome from "./components/welcome/Welcome";
+import Clock from "./components/clock/Clock";
+import Contact from "./components/contact/Contact";
+import NotFoundPage from "./components/notFoundPage/notFoundPage.js";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Switch>
+        <Route
+          exact
+          path="/"
+          render={(props) => <Welcome {...props} name="Josh" />}
+        />
+        <Route path="/contact" component={Contact} />
+        <Route path="/clock" component={Clock} />
+        <Route component={NotFoundPage} />
+        <Route path="/welcome:name" component={Welcome} />
+      </Switch>
     </div>
   );
 }
